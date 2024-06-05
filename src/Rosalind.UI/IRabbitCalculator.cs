1ab362dd9cb5f8fc925c840bafa13c42fcf89d91
Gerald Ncube <gncube@gmail.com>
2024-06-05 20:17:06 +0100

    Added failing test CalculateRabbitPairs_GivenMonthsAndOffspring_ReturnsTotalRabbitPairs

---
+++ src/Rosalind.UI/IRabbitCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace Rosalind.UI;
+public interface IRabbitCalculator
+{
+    long CalculateRabbitPairs(int months, int litterSize);
+}
+
+public class RabbitCalculator : IRabbitCalculator
+{
+    public long CalculateRabbitPairs(int months, int litterSize)
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ tests/RosalindSolverTests/RabbitTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Rosalind.UI;
+
+namespace RosalindSolverTests;
+public class RabbitTests
+{
+    [Fact]
+    public void CalculateRabbitPairs_GivenMonthsAndOffspring_ReturnsTotalRabbitPairs()
+    {
+        // Arrange
+        var months = 5;
+        var offspring = 3;
+        var expectedRabbitPairs = 29;
+        var rabbitCalculator = new RabbitCalculator();
+
+        // Act
+        var result = rabbitCalculator.CalculateRabbitPairs(months, offspring);
+
+        // Assert
+        result.Should().Be(expectedRabbitPairs);
+    }
+}
